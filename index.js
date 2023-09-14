317ef3303eafd27b72ab245be52c967c44c59dba
Angel Darco <angel92-eme@hotmail.com>
2023-09-14 12:10:53 -0500

    :fire: detect user default theme added

--- index.js
+++ index.js
@@ -3,35 +3,51 @@ document.addEventListener("DOMContentLoaded", () => {
   const darkContainer = document.querySelector(".dark-mode");
   const option = document.querySelectorAll(".dark-mode p");
 
+  defaultTheme();
+  
   img.addEventListener("click", () => {
     darkContainer.classList.toggle("hidden");
   });
-
   option.forEach((element) => {
-    element.addEventListener("click", darkMode);
+    element.addEventListener("click", darkModeSwitch);
   });
 });
 
-function darkMode(e) {
-  const root = document.querySelector(":root");
+const root = document.querySelector(":root");
+function darkModeSwitch(e) {
   switch (e.target.textContent) {
+    case "Use device theme":
+    defaultTheme();
+    break;
     case "Dark theme":
-      root.style.setProperty("--primary-color", "#0f0f0f");
-      root.style.setProperty("--text-color", "#f1f1f1");
-      root.style.setProperty("--secondary-text-color", "#aaaaaa");
-      root.style.setProperty("--buttons-color", "#ffffff14");
-      root.style.setProperty("--hover-buttons-color", "#303030");
-      root.style.setProperty("--blue-color", "#3ea6ff");
-      root.style.setProperty("--hover-blue-color", "#263850");
+      darkMode();
       break;
     case "Light theme":
-      root.style.setProperty("--primary-color", "#fff");
+      lightMode();
+      break;
+  }
+}
+
+function defaultTheme(){
+  const dark = window.matchMedia("(prefers-color-scheme: dark)");
+  if(dark.matches) darkMode();
+  else lightMode();
+  };
+function lightMode(){
+  root.style.setProperty("--primary-color", "#fff");
       root.style.setProperty("--text-color", "#000");
       root.style.setProperty("--secondary-text-color", "#606060");
       root.style.setProperty("--buttons-color", "#f2f2f2");
       root.style.setProperty("--hover-buttons-color", "#0000001a");
       root.style.setProperty("--blue-color", "#065fd4");
       root.style.setProperty("--hover-blue-color", "#def1ff");
-      break;
-  }
+}
+function darkMode() {
+  root.style.setProperty("--primary-color", "#0f0f0f");
+      root.style.setProperty("--text-color", "#f1f1f1");
+      root.style.setProperty("--secondary-text-color", "#aaaaaa");
+      root.style.setProperty("--buttons-color", "#ffffff14");
+      root.style.setProperty("--hover-buttons-color", "#303030");
+      root.style.setProperty("--blue-color", "#3ea6ff");
+      root.style.setProperty("--hover-blue-color", "#263850");
 }
